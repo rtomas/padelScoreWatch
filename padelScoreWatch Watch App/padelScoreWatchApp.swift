3f63f3a27a3ecbc4a777acfca27afc6429639e8e
Tomas R <rtomas@gmail.com>
2024-11-17 18:41:00 -0300

    Initial Commit

---
+++ padelScoreWatch Watch App/padelScoreWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  padelScoreWatchApp.swift
+//  padelScoreWatch Watch App
+//
+//  Created by tomtom on 17/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct padelScoreWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ padelScoreWatch Watch AppTests/padelScoreWatch_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  padelScoreWatch_Watch_AppTests.swift
+//  padelScoreWatch Watch AppTests
+//
+//  Created by tomtom on 17/11/2024.
+//
+
+import Testing
+@testable import padelScoreWatch_Watch_App
+
+struct padelScoreWatch_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
